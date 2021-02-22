fc2992844e9092d5e4c104f7f50be6bd2c1f88d8
devstefang <devstefang@gmail.com>
2021-02-22 18:32:47 +0200

    SoftUni

---
+++ CSharp-OOP/Homework-Lab/ReflectionAndAttributes/Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+   public class Spy
+    {
+        public string StealFieldInfo(string classToInvestigate, params string[] namesOfFields)
+        {
+            Type classType = Type.GetType(classToInvestigate);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Public |
+                   BindingFlags.Instance |
+                   BindingFlags.Static |
+                   BindingFlags.NonPublic);
+            StringBuilder sb = new StringBuilder();
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+            sb.AppendLine($"Class under investigation: {classToInvestigate}");
+            foreach (FieldInfo field in classFields.Where(f=> namesOfFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
