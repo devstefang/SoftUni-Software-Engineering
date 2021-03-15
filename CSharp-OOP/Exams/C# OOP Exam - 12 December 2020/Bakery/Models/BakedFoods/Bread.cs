df10b9cb4aa2c648c4b8aca0fbd948dee26ec6a3
devstefang <devstefang@gmail.com>
2021-03-15 18:13:38 +0200

    SoftUni

---
+++ CSharp-OOP/Exams/C# OOP Exam - 12 December 2020/Bakery/Models/BakedFoods/Bread.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.BakedFoods
+{
+    public class Bread : BakedFood
+    {
+        private const int breadPortion = 200;
+        public Bread(string name, decimal price) : base(name, breadPortion, price)
+        {
+        }
+    }
+}
